d8febe9ab831fb016cda0b9f0fbf18953b2e8a64
Otilia Toma <otiliatoma04@gmail.com>
2022-09-14 23:27:36 +0300

    comment

---
+++ PythonTutorial/app.py
@@ -0,0 +1,8 @@
+weight = input('What is your weight?')
+kg = 0.4 * int(weight)
+print(kg)
+
+name = input('What is your name? ')
+favorite_color = input('What is your favorite color? ')
+
+print(name + ' likes ' + favorite_color)
